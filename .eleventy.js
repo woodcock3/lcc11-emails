c39844aaa7ea99f3a412871abd1c5a495c7431b0
Paul <64870518+woodcock3@users.noreply.github.com>
2022-07-15 09:15:20 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,33 @@
 const juice = require("juice");
 const meta = require("./src/_data/meta");
 const now = String(Date.now());
+const svgContent = require('./shortcodes/svgcontent.js');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
 
   eleventyConfig.setWatchThrottleWaitTime(100);
 
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-  
-  // Shortcodes
+  /* SHORTCODES */
   // Add cache busting with {% version %} time string
   eleventyConfig.addShortcode('version', function () {
     return now
   });
-
+  
+  // Get current Year for text in footer
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  
+  // Insert any .svg file with {% icon "github" %} saved in .assets/images/svg/
+  eleventyConfig.addShortcode('svg', svgContent);
+  
+  // Insert spacer for emails
+  eleventyConfig.addShortcode(
+    "emailSpacer",
+    (size = "") =>
+      `<tr><td class="spacer${size ? `-${size}` : ""}">&#32;</td></tr>`
+  );
+  
+  /* FILTERS */
   eleventyConfig.addNunjucksAsyncFilter("emailHtml", (raw, callback) => {
     if (meta.environment === "prod") {
       callback(null, juice(raw));
@@ -22,12 +35,6 @@ module.exports = function (eleventyConfig) {
       callback(null, raw);
     }
   });
-
-  eleventyConfig.addShortcode(
-    "emailSpacer",
-    (size = "") =>
-      `<tr><td class="spacer${size ? `-${size}` : ""}">&#32;</td></tr>`
-  );
 
   return {
     dir: {
