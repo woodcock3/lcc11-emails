6b29a2ce473e2a688b03e7568875fe90f004b5de
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-12 11:42:36 +0000

    Update var-button.js

--- src/_includes/partials/var-button.js
+++ src/_includes/partials/var-button.js
@@ -1,7 +1,7 @@
 module.exports = async (code, btn-href, btn-text) => {
   return `<div>
   <!--[if mso]>
-    <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="{{ btn-href }}" style="v-text-anchor:middle; height: {{ meta.ctaButtonHeight }}; width: {{ meta.ctaButtonWidth }}" class="button-mso" arcsize="10%" stroke="f" fillcolor="{{ meta.ctaButtonColor }}">
+    <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="${btn-href}" style="v-text-anchor:middle; height: {{ meta.ctaButtonHeight }}; width: {{ meta.ctaButtonWidth }}" class="button-mso" arcsize="10%" stroke="f" fillcolor="{{ meta.ctaButtonColor }}">
     <w:anchorlock/>
       <center>
         <![endif]-->
