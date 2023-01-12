04deac69aa3cc9438f2f1428642666021fd83564
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-12 11:40:47 +0000

    Update and rename var-button.html to var-button.js

--- src/_includes/partials/var-button.js
+++ src/_includes/partials/var-button.js
@@ -1,13 +1,15 @@
-<div>
+module.exports = async (code, btn-href, btn-text) => {
+  return `<div>
   <!--[if mso]>
     <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="{{ btn-href }}" style="v-text-anchor:middle; height: {{ meta.ctaButtonHeight }}; width: {{ meta.ctaButtonWidth }}" class="button-mso" arcsize="10%" stroke="f" fillcolor="{{ meta.ctaButtonColor }}">
     <w:anchorlock/>
       <center>
         <![endif]-->
-          <a class="button" href="{{ btn-href }}">{{ btn-text }}</a>
+          <a class="button" href="${btn-href}">${btn-text}</a>
         <!--[if mso]>
       </center>
     </v:roundrect>
   <![endif]-->
-</div>
-<br>
+  </div>
+  <br>`;
+});
