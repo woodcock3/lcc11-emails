de9c63574d63991c753964187b7792da9c3ed6a6
Paul <64870518+woodcox@users.noreply.github.com>
2023-01-13 09:25:25 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("var-btn", function(btnHref, btnText) {
     return `<div>
     <!--[if mso]>
-      <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="${btnHref}" style="v-text-anchor:middle; height: meta.ctaButtonHeight; width: meta.ctaButtonWidth" class="button-mso" arcsize="10%" stroke="f" fillcolor="meta.ctaButtonColor">
+      <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="${btnHref}" style="v-text-anchor:middle; height: ${meta.ctaButtonHeight}; width: ${meta.ctaButtonWidth}" class="button-mso" arcsize="10%" stroke="f" fillcolor="${meta.ctaButtonColor}">
       <w:anchorlock/>
         <center>
           <![endif]-->
